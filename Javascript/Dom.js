46e111d67a67cbcbda97b2a069a25a8d1b6e482f
Pranav Waingade <pranavwaingade25@gmail.com>
2026-01-20 10:13:46 +0530

    Add files via upload

---
+++ Javascript/Dom.js
@@ -0,0 +1,94 @@
+// let element = document.getElementById("heading");
+// console.log(element);
+
+// innertext vs textcontent
+
+// console.log(element.innerText);
+// console.log(element.textContent);
+// element.textContent = "Ghar jao jaldi";
+
+// console.log(element.innerHTML);
+// element.innerHTML = "Chalo ruk jao";
+// element.style.color = "blue";
+
+// let Name = prompt("Enter your name");
+// if (Name) {
+//   element.innerText = "Good Morning!!!!" + Name;
+// } else {
+//   element.innerText = "Good Morning!!!!" + " User ";
+// }
+
+// let paragraph = document.getElementById("para");
+// console.log(paragraph);
+
+// let output = document.getElementsByClassName("heading2");
+// console.log(output);
+
+// output[0].innerHTML = "Pankaj";
+// for (let i = 0; i < output.length; i++) {
+//   output[i].style.color = "blue";
+// }
+// output[1].style.background = "pink";
+
+// let element = document.getElementsByTagName("p");
+// console.log(element);
+
+// let element = document.querySelector("#heading");
+// console.log(element);
+
+// let element2 = document.querySelectorAll(".heading2");
+// console.log(element2);
+
+// let element3 = document.querySelectorAll("p");
+// console.log(element3);
+
+// let display = document.querySelector(".screen");
+// let element = document.createElement("h1");
+// let element2 = document.createElement("h2");
+// let element3 = document.createElement("h3");
+
+// element.innerText = "I am Dynamic one";
+// element2.innerHTML = "I am Happy shappy";
+// element3.textContent = "Good Morning mitron!!!";
+
+// console.log(element);
+// display.appendChild(element);
+// display.appendChild(element2);
+
+// display.append(element, element2);
+// display.prepend(element3);
+
+// ================================================================
+
+// let data = Number(prompt("Enter Your salary"));
+// let display = document.querySelector(".screen");
+
+// if (data > 50000) {
+//   let bonus = document.createElement("h1");
+//   bonus.innerText = `You will get 10,000 joining bonus`;
+//   display.appendChild(bonus);
+// } else {
+//   let noBonus = document.createElement("p");
+//   noBonus.innerText = `Bhai mehnat karo`;
+//   display.appendChild(noBonus);
+// }
+
+// ================================================================
+
+
+// login/ Signup
+
+// ================================================================
+
+
+// let orders = ["Laptop", "Phone", "Headphone", "Charger", "Mixy"];
+
+// let display = document.querySelector("#OrderList");
+
+// for (let i = 0; i < orders.length; i++) {
+//   let element = document.createElement("li");
+//   element.innerText = orders[i];
+//   display.append(element);
+// }
+
+// classlist;
