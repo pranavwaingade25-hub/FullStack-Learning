7a7ba147b2fc1da307beb708c3fc9bb22464b616
Pranav Waingade <pranavwaingade25@gmail.com>
2026-01-22 09:42:56 +0530

    Add files via upload

---
+++ asynchronous javascript/intro.js
@@ -0,0 +1,44 @@
+console.log("Hello, Async JavaScript!");
+
+// setTimeout /  setInterval ---> Async JS
+// Api ---> function -->async -> time -> data ->promise(object)
+
+// console.log("Started the exe");
+
+// setInterval(() => {
+//   console.log(" I am interval");
+// }, 3000);
+
+// console.log("Code ended");
+
+// console.log("Template of body section");
+// setTimeout(() => {
+//   console.log("APi called for body");
+// }, 3000); //5sec
+
+// console.log("Template for Menu");
+// setTimeout(() => {
+//   console.log("Api for Menu");
+// }, 3000);
+// // 3sec
+
+// console.log("Template for Footer section");
+// setTimeout(() => {
+//   console.log("Api for Footer");
+// }, 3000);
+// // 10 sec
+
+// console.log("Execution end!!");
+
+
+//settimeout -> 3sec
+//Api ->3sec->priority ? ->Promise 
+
+//callback hell / promise /promise chaining/ async await
+
+
+
+
+
+
+//fetch api js
